9e1cf0787dbc5f507524c16d850644011f9587ac
Asher Yartsev <asheryartsev@campus.technion.ac.il>
2019-05-29 16:55:06 +0300

    made basic actor model , working

--- tf/actor/actor.py
+++ tf/actor/actor.py
@@ -1,14 +1,16 @@
 
-
+from abc import ABC, abstractmethod
+import tensorflow as tf
+import numpy as np
 
 
 class actorBasedNN(ABC):
     def __init__(self):
         super().__init__()
 
-    @abstractmethod
-    def verbose(self):
-        pass
+    # @abstractmethod
+    # def verbose(self):
+    #     pass
 
 class TfGraphTracker(actorBasedNN):
     def __init__(self):
@@ -30,7 +32,7 @@ class fullyConnectedLayerBuilder(actorBasedNN):
         self.settings = fullyConnectedSettings
 
     def construct(self,inputLayer,nameScope):
-        with tf.name_scope("actor"):
+        with tf.name_scope(nameScope):
             W_fc = tf.Variable(tf.truncated_normal(self.settings['weightMatrixShape'],
                                        stddev=self.settings['stddev']))
             b_fc = tf.Variable(tf.constant(0.0, shape=self.settings['biasShape']))
@@ -38,3 +40,26 @@ class fullyConnectedLayerBuilder(actorBasedNN):
             nonLinearity = tf.nn.relu(tf.add(tf.transpose(opMatMul), b_fc))
 
             return nonLinearity
+
+class actorNNBuilder(actorBasedNN):
+    def __init__(self):
+        self.graphTracker = TfGraphTracker()
+
+    def construct(self,inputPlaceholders):
+        fc_l1Config = {'weightMatrixShape': [100, 200], 'biasShape': [100], 'stddev': 0.03}
+        fc_l1 = fullyConnectedLayerBuilder(fc_l1Config).construct(inputPlaceholders, 'Layer1')
+        self.graphTracker.track(fc_l1, 'Layer1')
+        return self.graphTracker, fc_l1
+
+
+
+
+params = np.ones([2,200])
+inputPlaceholder = tf.placeholder(tf.float32, shape=(None,200), name='inputToNN')
+graphTracker, actorNNTop = actorNNBuilder().construct(inputPlaceholder)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    fcn = sess.run([actorNNTop], feed_dict={inputPlaceholder: params})
+    print(fcn)
+
--- tf/actorImprovalProcess.py
+++ tf/actorImprovalProcess.py
@@ -1,14 +1,3 @@
 import tensorflow as tf
 import numpy as np
 import actor
-
-params = np.ones([2,1200])
-with tf.name_scope('actorImprovalProcess'):
-
-    actor = actor.actor()
-    topNode = actor.debug_getBodyHead()
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-
-        fcn = sess.run([topNode], feed_dict={actor.actorTfNodes['NN_feeds']['batchStateInput']: params})
-        print(fcn)
